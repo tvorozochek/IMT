6243b7e3c336be31161a2c724363eccdb9d3d020
tvorozochek <89125683472i@gmail.com>
2025-11-06 09:55:47 +0300

    Add files via upload

---
+++ Client_txt.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+struct Person
+{
+    string name;
+    int height;
+    int weight;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const char* nameR = "G:\\for_serv\\Request.txt";
+    const char* nameA = "G:\\for_serv\\Answer.txt";
+
+    int prev_lines = 0;
+
+    while (true)
+    {
+        Person A;
+        cout << "Введите запрос: Фамилия Рост Вес\n";
+        cin >> A.name >> A.height >> A.weight;
+
+        // добавляем запись в конец реквест
+        ofstream f_REQ(nameR, ios::app);
+        f_REQ << A.name << " " << A.height << " " << A.weight << endl;
+        f_REQ.close();
+
+        // ждём ответ
+        int lines_now = 0;
+        string last_line;
+        while (true)
+        {
+            ifstream fA(nameA);
+            lines_now = 0;
+            last_line.clear();
+
+            string line;
+            while (getline(fA, line))
+            {
+                lines_now++;
+                last_line = line;
+            }
+            fA.close();
+
+            if (lines_now > prev_lines)
+            {
+                cout << "Ответ сервера: " << last_line << endl;
+                prev_lines = lines_now;
+                break;
+            }
+
+            Sleep(500);
+        }
+    }
+
+    return 0;
+}
---
+++ Server_txt.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+struct Person
+{
+    string name;
+    int height;
+    int weight;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const char* nameR = "G:\\for_serv\\Request.txt";
+    const char* nameA = "G:\\for_serv\\Answer.txt";
+
+    cout << "Server is working..." << endl;
+
+    // очистим файлы при запуске
+    {
+        ofstream clearR(nameR, ios::trunc);
+        ofstream clearA(nameA, ios::trunc);
+        clearA.close();
+        clearR.close();
+    }
+
+    // запоминаем, сколько строк уже обработано
+    int processed_lines = 0;
+
+    while (true)
+    {
+        ifstream fR(nameR);
+        string name;
+        int height, weight;
+        int current_line = 0;
+
+        // читаем построчно
+        while (fR >> name >> height >> weight)
+        {
+            current_line++;
+            if (current_line <= processed_lines)
+                continue; // пропускаем старые записи
+
+            // обработка
+            double IMT = weight / (0.01 * height) / (0.01 * height);
+            string result;
+
+            if (IMT < 18.5)
+                result = "Недостаток веса";
+            else if (IMT < 25)
+                result = "Норма веса";
+            else
+                result = "Избыток веса";
+
+            cout << "Получено: " << name << "  Рост: " << height << "  Вес: " << weight << "  IMT: " << IMT << endl;
+
+            // записываем ответ
+            ofstream fA(nameA, ios::app);
+            fA << result << endl;
+        }
+
+        processed_lines = current_line;
+        fR.close();
+
+        Sleep(500); // проверяем файл каждые полсекунды
+    }
+
+    return 0;
+}
